c31f102827d8b21ec657da753241f3bed213e702
mshirdel <mshirdel@gmail.com>
2020-11-19 17:49:43 +0330

    add user auth with devise

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,4 @@
 class MainController < ApplicationController
-  def index
-  end
-
   def about
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'main#index', as: 'homepage'
+  devise_for :users
+  root "dashboard#index", as:'homepage'
   get 'main/about'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
